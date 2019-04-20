44349d5e6a3ae1b59b718334c970c257f3edce1d
Simon Guetta <simonguetta1996@gmail.com>
2019-04-20 11:06:22 +0200

    change middleware allowing

--- routes/web.php
+++ routes/web.php
@@ -19,21 +19,21 @@ Route::get('/', function () {
 Route::view('/', 'welcome');
 
 Route::get('/entreprise', 'EntreprisesController@index')->middleware('auth');
-Route::get('/entreprise/create', 'EntreprisesController@create')->middleware('auth', 'admin', 'gestionnaire');
+Route::get('/entreprise/create', 'EntreprisesController@create')->middleware('auth', 'gestionnaire');
 Route::post('/entreprise', 'EntreprisesController@store')->middleware('auth');
 Route::get('/entreprise/{id}', 'EntreprisesController@show')->middleware('auth');
-Route::get('/entreprise/update/{id}', 'EntreprisesController@edit')->middleware('auth', 'admin', 'gestionnaire');
+Route::get('/entreprise/update/{id}', 'EntreprisesController@edit')->middleware('auth', 'gestionnaire');
 Route::put('/entreprise/{id?}', 'EntreprisesController@update')->middleware('auth');
 Route::get('/entreprise/delete/{id}', 'EntreprisesController@delete')->middleware('auth', 'admin');
 Route::delete('/entreprise/{id?}', 'EntreprisesController@destroy')->middleware('auth');
 Route::get('/entreprise', 'EntreprisesController@search')->middleware('auth');
 
 Route::get('/collaborateur', 'CollaborateursController@index')->middleware('auth');
-Route::get('/collaborateur/create', 'CollaborateursController@create')->middleware('auth', 'admin', 'gestionnaire');
+Route::get('/collaborateur/create', 'CollaborateursController@create')->middleware('auth', 'gestionnaire');
 Route::post('/collaborateur/', 'CollaborateursController@store')->middleware('auth');
 Route::get('/collaborateur/{id}', 'CollaborateursController@index')->middleware('auth');
 Route::put('/collaborateur/{id?}', 'CollaborateursController@update')->middleware('auth');
-Route::get('/collaborateur/update/{id}', 'CollaborateursController@edit')->middleware('auth', 'admin', 'gestionnaire');
+Route::get('/collaborateur/update/{id}', 'CollaborateursController@edit')->middleware('auth', 'gestionnaire');
 Route::put('/collaborateur/{id?}', 'CollaborateursController@update')->middleware('auth');
 Route::get('/collaborateur/delete/{id}', 'CollaborateursController@delete')->middleware('auth', 'admin');
 Route::delete('/collaborateur/{id?}', 'CollaborateursController@destroy')->middleware('auth');
